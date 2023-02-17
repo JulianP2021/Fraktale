8d31408c5924b0c3fec720eb1ce1c6a22a8fa6b5
JulianP2021 <julian@scharf.am>
2023-02-17 22:08:28 +0100

    MAndelbrot,Apfelmännchen, Seepferdchen funktionieren

--- index.js
+++ index.js
@@ -33,15 +33,45 @@ let seepferdchen = {
 	aK: 10,
 	formel: "",
 };
+let apfelmännchen = {
+	kantenlänge: 0.0000350911,
+	xM: 0.225305566,
+	yM: 0.840426025,
+	startX: 0,
+	startY: 0,
+	anzahlIterationen: 1131,
+	aK: 100,
+	formel: "",
+};
 //TODO
 let sternhaufen = {
-	kantenlänge: 2,
+	kantenlänge: 0.5,
 	xM: 0,
 	yM: 0,
 	startX: -0.221346489,
 	startY: 1.464902383,
 	anzahlIterationen: 100,
 	aK: 100,
+	formel: "",
+};
+let blueMoon = {
+	kantenlänge: 0.0164069,
+	xM: -0.9355857,
+	yM: -0.171,
+	startX: 0,
+	startY: 0,
+	anzahlIterationen: 100,
+	aK: 10,
+	formel: "",
+};
+let schleife = {
+	kantenlänge: 2.24,
+	xM: 7.848,
+	yM: 4.8,
+	startX: 0,
+	startY: 0,
+	anzahlIterationen: 50,
+	aK: 100000000,
 	formel: "",
 };
 
@@ -58,6 +88,18 @@ fraktalinput.addEventListener("input", ev => {
 	if (fraktalinput.value == "Seepferdchen") {
 		ausgewählt = seepferdchen;
 	}
+	if (fraktalinput.value == "Apfelmännchen") {
+		ausgewählt = apfelmännchen;
+	}
+	if (fraktalinput.value == "Schleife") {
+		ausgewählt = schleife;
+	}
+	if (fraktalinput.value == "Sternhaufen") {
+		ausgewählt = sternhaufen;
+	}
+	if (fraktalinput.value == "Blauer Mond") {
+		ausgewählt = blueMoon;
+	}
 	kantenlängeinput.value = ausgewählt.kantenlänge;
 	xMinput.value = ausgewählt.xM;
 	yMinput.value = ausgewählt.yM;
@@ -73,6 +115,18 @@ drawbtn.addEventListener("click", ev => {
 	if (fraktalinput.value == "Seepferdchen") {
 		ausgewählt = seepferdchen;
 	}
+	if (fraktalinput.value == "Apfelmännchen") {
+		ausgewählt = apfelmännchen;
+	}
+	if (fraktalinput.value == "Schleife") {
+		ausgewählt = schleife;
+	}
+	if (fraktalinput.value == "Sternhaufen") {
+		ausgewählt = sternhaufen;
+	}
+	if (fraktalinput.value == "Blauer Mond") {
+		ausgewählt = blueMoon;
+	}
 	ausgewählt.kantenlänge = kantenlängeinput.value;
 	ausgewählt.xM = xMinput.value;
 	ausgewählt.yM = yMinput.value;
@@ -81,14 +135,13 @@ drawbtn.addEventListener("click", ev => {
 	draw();
 });
 
-let ausgewählt = seepferdchen;
+let ausgewählt = mandelbrot;
 kantenlängeinput.value = ausgewählt.kantenlänge;
 xMinput.value = ausgewählt.xM;
 yMinput.value = ausgewählt.yM;
 anzahlIterationeninput.value = ausgewählt.anzahlIterationen;
 aKinput.value = ausgewählt.aK;
 
-console.log(Math.tanh(90 * Math.PI));
 resizeCanvas();
 
 console.log("ready");
@@ -114,10 +167,15 @@ function draw(codeAsString) {
 				iteration < ausgewählt.anzahlIterationen &&
 				Math.sqrt(r * r + i * i) < ausgewählt.aK
 			) {
-				iteration++;
-				let iterationsberechnung = iterationBerechnen(r, i, codeAsString);
+				let iterationsberechnung = iterationBerechnen(
+					r,
+					i,
+					codeAsString,
+					iteration
+				);
 				r = iterationsberechnung.r;
 				i = iterationsberechnung.i;
+				iteration++;
 			}
 			if (iteration == ausgewählt.anzahlIterationen) {
 				ctx.fillStyle = "white";
@@ -139,29 +197,128 @@ function draw(codeAsString) {
 	console.log("Fertig gezeichnet");
 }
 
-function iterationBerechnen(r, i, codeAsString) {
+function iterationBerechnen(r, i, codeAsString, iteration) {
 	if (ausgewählt == seepferdchen) {
-		r2 = r * r - i * i;
-		i2 = 2 * r * i;
+		let r2 = r * r - i * i;
+		let i2 = 2 * r * i;
+		r = r2;
+		i = i2;
+		r = r - iC;
+		i = i + rC;
+	} else if (ausgewählt == apfelmännchen) {
+		let r2 = r * r - i * i;
+		let i2 = 2 * r * i;
 		r = r2;
 		i = i2;
 		r = r - iC;
 		i = i + rC;
 	} else if (ausgewählt == mandelbrot) {
-		r2 = r * r - i * i;
-		i2 = 2 * r * i;
+		let r2 = r * r - i * i;
+		let i2 = 2 * r * i;
 		r = r2;
 		i = i2;
 		r = r + rC;
 		i = i + iC;
+	} else if (ausgewählt == schleife) {
+		if (iteration == 0) {
+			r = rC;
+			i = iC;
+		}
+		//Potenzieren von komplexen Zahlen https://www.youtube.com/watch?v=rXM9GXxa-cU
+		let phi = 0;
+		if (!isNaN(Math.acos(r / Math.sqrt(r * r + i * i)))) {
+			phi = Math.acos(r / Math.sqrt(r * r + i * i));
+		}
+		const z_5_r = Math.pow(r, 5) * Math.cos(5 * phi);
+		const z_5_i = Math.pow(r, 5) * Math.sin(5 * phi);
+		const z_4_r = Math.pow(r, 4) * Math.cos(4 * phi);
+		const z_4_i = Math.pow(r, 4) * Math.sin(4 * phi);
+		//dividieren von komplexen Zahlen https://www.mathebibel.de/komplexe-zahlen-dividieren
+		let nenner_R = z_5_r - 2 * r - 1 + z_5_i * i;
+		let zähler_R = 5 * z_4_r - 2;
+		let nenner_I = z_5_i - 2 * i + z_5_i * r;
+		let zähler_I = 5 * z_4_i - 2;
+		r =
+			r -
+			(nenner_R * zähler_R + nenner_I * -zähler_I) /
+				(nenner_R * nenner_R - nenner_I * nenner_I);
+		i =
+			i -
+			(nenner_R * -zähler_I + nenner_I * zähler_R) /
+				(nenner_R * nenner_R - nenner_I * nenner_I);
+		console.log(
+			"i: ",
+			i,
+			"r: ",
+			r,
+			"phi: ",
+			phi,
+			"zhoch5r: ",
+			z_5_r,
+			"zhoch5i: ",
+			z_5_i,
+			z_4_r,
+			z_4_i
+		);
 	} else if (ausgewählt == sternhaufen) {
-		const e_2iz = Math.exp(2 * i * r);
-		const resultReal = r * (e_2iz - 1) / (e_2iz + 1);
-		const resultImg = i * (e_2iz - 1) / (e_2iz + 1);
-		r = resultReal;
-		i = resultImg;
-		r = r + rC;
-		i = i + iC;
+		let phi = 0;
+		if (!isNaN(Math.acos(r / Math.sqrt(r * r + i * i)))) {
+			phi = Math.acos(r / Math.sqrt(r * r + i * i));
+		}
+		r = r * Math.tan(phi);
+		i = i * Math.tan(phi);
+		r = r - iC;
+		i = i + rC;
+	} else if (ausgewählt == blueMoon) {
+		//@TODO
+		let zminusC_3_r =
+			Math.pow(r, 3) *
+			Math.cos(
+				3 *
+					Math.acos(
+						(r - rC) / Math.sqrt((r - rC) * (r - rC) + (i - iC) * (i - iC))
+					)
+			);
+		let zminusC_3_i =
+			Math.pow(r, 3) *
+			Math.sin(
+				3 *
+					Math.acos(
+						(r - rC) / Math.sqrt((r - rC) * (r - rC) + (i - iC) * (i - iC))
+					)
+			);
+		let zminusiMalc_2_r =
+			Math.pow(r, 2) *
+			Math.cos(
+				2 *
+					Math.acos(
+						(r + iC) / Math.sqrt((r + iC) * (r + iC) + (i - rC) * (i - rC))
+					)
+			);
+		let zminusiMalc_2_i =
+			Math.pow(r, 2) *
+			Math.sin(
+				2 *
+					Math.acos(
+						(r + iC) / Math.sqrt((r + iC) * (r + iC) + (i - rC) * (i - rC))
+					)
+			);
+		//dividieren von komplexen Zahlen https://www.mathebibel.de/komplexe-zahlen-dividieren
+		let nenner_R = zminusC_3_r - 2 * (r - rC);
+		let zähler_R = 3 * zminusiMalc_2_r;
+		let nenner_I = zminusC_3_i - 2 * (i - iC) - 4;
+		let zähler_I = 3 * zminusiMalc_2_i;
+		r =
+			r -
+			rC -
+			(nenner_R * zähler_R + nenner_I * -zähler_I) /
+				(nenner_R * nenner_R - nenner_I * nenner_I);
+		i =
+			i -
+			iC -
+			(nenner_R * -zähler_I + nenner_I * zähler_R) /
+				(nenner_R * nenner_R - nenner_I * nenner_I);
+		//console.log("i: ",i,"r: ", r, "zhoch5r: ",zminusC_3_r, "zhoch5i: ",zminusC_3_i, zminusiMalc_2_r, zminusiMalc_2_i);
 	} else if (codeAsString != "") {
 		eval(codeAsString);
 	}
